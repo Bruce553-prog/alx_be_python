5116c4e2407611ac24675bd96f675051d0e29192
bruce wyllis <brucewyllis01@gmail.com>
2025-07-14 13:57:40 +0300

    Updated basic_operations.py with correct output

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 number1=10
 number2=5
-sum=number1+number2
-print(f"Addition of {number1} and {number2} is {sum}")
+addition=number1+number2
+print(f"Addition of {number1} and {number2} is {addition}")
 subtraction= number1-number2
 print(f"Subtraction of {number1} and {number2} is {subtraction}") 
 multiplication= number1*number2
