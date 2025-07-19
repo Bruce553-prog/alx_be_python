5533725890bb59b03761976ea4dd1135421a73e2
bruce wyllis <brucewyllis01@gmail.com>
2025-07-19 15:58:42 +0300

    changed files

--- multiplication_table.py
+++
@@ -1,4 +0,0 @@
-number=int(input("enter a  number to see its multiplication table:. "))
-for i in range(1,11):
-    result=number * i
-    print(f"{number} * {i} = {result} ")
--- pattern_drawing.py
+++
@@ -1,7 +0,0 @@
-symbol=input("what symbol do you love  ")
-number=int(input("Enter positive interger  "))
-for i in range (number):
-    for j in range(number):
-        print(symbol,end=" ")
-    print()
-    
