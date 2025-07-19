88809f30fe92fa9bdf4c9f946fc5809ef9993223
bruce wyllis <brucewyllis01@gmail.com>
2025-07-19 14:47:05 +0300

    minor changes

---
+++ pattern_drawing.py
@@ -0,0 +1,7 @@
+symbol=input("what symbol do you love  ")
+number=int(input("Enter positive interger  "))
+for i in range (number):
+    for j in range(number):
+        print(symbol,end=" ")
+    print()
+    
