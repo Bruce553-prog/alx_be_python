cd7f42097233a4554e436b94175b24bc282558ce
bruce wyllis <brucewyllis01@gmail.com>
2025-07-19 16:41:06 +0300

    small change on the second line

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,5 @@
 symbol=input("what symbol do you love  ")
-number=int(input("Enter positive interger  "))
+number=int(input("Enter the size of the pattern: "))
 for i in range (number):
     for j in range(number):
         print(symbol,end=" ")
