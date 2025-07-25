2df241261ff23ae16648a375fab7b64f8eb27e29
bruce wyllis <brucewyllis01@gmail.com>
2025-07-25 21:04:55 +0300

    deleted the unwanted file

---
+++ fns_and_dsa/control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+symbol=input("what symbol do you love  ")
+number=int(input("Enter the size of the pattern: "))
+i = 0
+while i < number:
+    j = 0
+    while j < number:
+        print("*", end=" ")
+        j += 1
+    print()
+    i += 1
+
+    
